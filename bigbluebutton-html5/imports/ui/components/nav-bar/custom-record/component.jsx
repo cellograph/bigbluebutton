9413108d51fcc55f5808931bbecb1a73c923fb30
Imran Hossain <ahmedrakibimran@gmail.com>
2020-11-26 01:43:00 +0600

    Added RecordButton

--- bigbluebutton-html5/imports/ui/components/nav-bar/custom-record/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/custom-record/component.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import useMediaRecorder from "@wmik/use-media-recorder";
 
+function Player({ srcBlob, audio }) {
+  if (!srcBlob) {
+    return null;
+  }
+
+  if (audio) {
+    return <audio src={URL.createObjectURL(srcBlob)} controls />;
+  }
+
+  return (
+    <video
+      style={{ display: "none" }}
+      src={URL.createObjectURL(srcBlob)}
+      width={520}
+      height={480}
+      controls
+    />
+  );
+}
+
 function ScreenRecorderApp(props) {
   let {
     error,
@@ -29,31 +49,34 @@ function ScreenRecorderApp(props) {
   };
 
   return (
-    <div>
+    <article>
+      <h1>Screen recorder</h1>
       {error ? `${status} ${error.message}` : status}
-
-      <button
-        type="button"
-        onClick={getMediaStream}
-        disabled={status === "ready"}
-      >
-        Share screen
-      </button>
-      <button
-        type="button"
-        onClick={startRecording}
-        disabled={status === "recording"}
-      >
-        Start recording
-      </button>
-      <button
-        type="button"
-        onClick={(e) => handleStopRecord(e)}
-        disabled={status !== "recording"}
-      >
-        Stop recording
-      </button>
-    </div>
+      <section>
+        <button
+          type="button"
+          onClick={getMediaStream}
+          disabled={status === "ready"}
+        >
+          Share screen
+        </button>
+        <button
+          type="button"
+          onClick={startRecording}
+          disabled={status === "recording"}
+        >
+          Start recording
+        </button>
+        <button
+          type="button"
+          onClick={stopRecording}
+          disabled={status !== "recording"}
+        >
+          Stop recording
+        </button>
+      </section>
+      <Player srcBlob={mediaBlob} />
+    </article>
   );
 }
 
